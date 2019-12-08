10c13efc942c5002a71e0b24620e677ecde134ef
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2019-12-08 21:36:22 +0100

    Initial commit

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+    [SerializeField]
+    float initialVelocity = 6;
+
+    [SerializeField]
+    float lifeTime = 4;
+
+	void Start ()
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = transform.forward * initialVelocity;
+
+        Destroy(this.gameObject, lifeTime);
+	}
+}
