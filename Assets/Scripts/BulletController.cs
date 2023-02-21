b3207c6f65e27c2bc74c22391d08edd17b36aad9
theo <theo@zibell.se>
2023-02-21 21:45:00 +0100

    first

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -1,20 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class BulletController : MonoBehaviour {
+public class BulletController : NetworkBehaviour
+{
 
-  [SerializeField]
-  float initialVelocity = 6;
+    [SerializeField]
+    float initialVelocity = 6;
 
-  [SerializeField]
-  float lifeTime = 4;
+    [SerializeField]
+    float lifeTime = 4;
 
-	void Start ()
-  {
-    Rigidbody rigidbody = GetComponent<Rigidbody>();
-    rigidbody.velocity = transform.forward * initialVelocity;
+    void Start()
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = transform.forward * initialVelocity;
 
-    Destroy(this.gameObject, lifeTime);
-	}
+        Destroy(this.gameObject, lifeTime);
+    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,51 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : NetworkBehaviour
+{
 
-  [SerializeField]
-  float movementSpeed = 3f; // Unity-enheter per sekund
+    [SerializeField]
+    float movementSpeed = 3f; // Unity-enheter per sekund
 
-  [SerializeField]
-  float rotationSpeed = 150f; // Grader per sekund
+    [SerializeField]
+    float rotationSpeed = 150f; // Grader per sekund
 
-  [SerializeField]
-  GameObject bulletPrefab;
+    [SerializeField]
+    GameObject bulletPrefab;
 
-  [SerializeField]
-  Transform bulletSpawnPoint;
+    [SerializeField]
+    Transform bulletSpawnPoint;
 
-  [SerializeField]
-  float timeBetweenShots = 0.5f;
-  float timeSinceLastShot = 0f;
-	
-	void Update () {
+    [SerializeField]
+    float timeBetweenShots = 0.5f;
+    float timeSinceLastShot = 0f;
 
-    float yRotation = Input.GetAxisRaw("Horizontal") * rotationSpeed * Time.deltaTime;
-    float zMovement = Input.GetAxisRaw("Vertical") * movementSpeed * Time.deltaTime;
-
-    Vector3 rotationVector = new Vector3(0, yRotation, 0);
-    Vector3 movementVector = new Vector3(0, 0, zMovement);
-
-    transform.Rotate(rotationVector);
-    transform.Translate(movementVector);
-
-    timeSinceLastShot += Time.deltaTime;
-
-    if (Input.GetAxisRaw("Fire1") > 0)
+    public override void OnStartLocalPlayer()
     {
-      if (timeSinceLastShot > timeBetweenShots)
-      {
-        Fire();
-        timeSinceLastShot = 0;
-      }
+        GetComponent<Renderer>().material.color = Color.green;
     }
 
-	}
+    void Update()
+    {
+        if (!isLocalPlayer) return;
 
-  void Fire()
-  {
-    GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-  }
+        float yRotation = Input.GetAxisRaw("Horizontal") * rotationSpeed * Time.deltaTime;
+        float zMovement = Input.GetAxisRaw("Vertical") * movementSpeed * Time.deltaTime;
+
+        Vector3 rotationVector = new Vector3(0, yRotation, 0);
+        Vector3 movementVector = new Vector3(0, 0, zMovement);
+
+        transform.Rotate(rotationVector);
+        transform.Translate(movementVector);
+
+        timeSinceLastShot += Time.deltaTime;
+
+        if (Input.GetAxisRaw("Fire1") > 0)
+        {
+            if (timeSinceLastShot > timeBetweenShots)
+            {
+                Fire();
+                timeSinceLastShot = 0;
+            }
+        }
+
+    }
+
+    void Fire()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+    }
 }
