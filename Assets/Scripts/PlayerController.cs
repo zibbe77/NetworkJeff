7ef6dceb2da4c1c8a35a0b97253ebb195da33d97
zibbe77 <91542247+zibbe77@users.noreply.github.com>
2023-03-07 13:09:59 +0100

    uppdate

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -53,8 +53,10 @@ public class PlayerController : NetworkBehaviour
 
     }
 
+    [Command]
     void Fire()
     {
         GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        NetworkServer.Spawn(bullet);
     }
 }
